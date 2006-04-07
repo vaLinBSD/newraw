0f4348698c02992e741be657f3fb5458a6569517
misiunio <misiunio@d66f2c29-dc10-0410-be5d-887fce32e556>
2006-04-07 21:27:51 +0000

    Added high qiality 2x and 3x scalers
    git-svn-id: https://newraw.svn.sourceforge.net/svnroot/newraw@2 d66f2c29-dc10-0410-be5d-887fce32e556

--- sdlstub.cpp
+++ sdlstub.cpp
@@ -70,13 +70,19 @@ struct SDLStub : SystemStub {
 	void point1x(uint16 *dst, uint16 dstPitch, const uint16 *src, uint16 srcPitch, uint16 w, uint16 h);
 	void point2x(uint16 *dst, uint16 dstPitch, const uint16 *src, uint16 srcPitch, uint16 w, uint16 h);
 	void point3x(uint16 *dst, uint16 dstPitch, const uint16 *src, uint16 srcPitch, uint16 w, uint16 h);
+	void scale2x(uint16 *dst, uint16 dstPitch, const uint16 *src, uint16 srcPitch, uint16 w, uint16 h);
+	void scale3x(uint16 *dst, uint16 dstPitch, const uint16 *src, uint16 srcPitch, uint16 w, uint16 h);
+	
 };
 
 const SDLStub::Scaler SDLStub::_scalers[] = {
 	{ "Point1x", &SDLStub::point1x, 1 },
 	{ "Point2x", &SDLStub::point2x, 2 },
-	{ "Point3x", &SDLStub::point3x, 3 }
+	{ "Scale2x", &SDLStub::scale2x, 2 },
+	{ "Point3x", &SDLStub::point3x, 3 },
+	{ "Scale3x", &SDLStub::scale3x, 3 }
 };
+
 
 SystemStub *SystemStub_SDL_create() {
 	return new SDLStub();
@@ -368,6 +374,74 @@ void SDLStub::point3x(uint16 *dst, uint16 dstPitch, const uint16 *src, uint16 sr
 			*(p + 0 + dstPitch * 2) = c;
 			*(p + 1 + dstPitch * 2) = c;
 			*(p + 2 + dstPitch * 2) = c;
+		}
+		dst += dstPitch * 3;
+		src += srcPitch;
+	}
+}
+
+void SDLStub::scale2x(uint16 *dst, uint16 dstPitch, const uint16 *src, uint16 srcPitch, uint16 w, uint16 h) {
+	dstPitch >>= 1;
+	while (h--) {
+		uint16 *p = dst;
+		for (int i = 0; i < w; ++i, p += 2) {
+			uint16 B = *(src + i - srcPitch);
+			uint16 D = *(src + i - 1);
+			uint16 E = *(src + i);
+			uint16 F = *(src + i + 1);
+			uint16 H = *(src + i + srcPitch);
+			if (B != H && D != F) {
+				*(p) = D == B ? D : E;
+				*(p + 1) = B == F ? F : E;
+				*(p + dstPitch) = D == H ? D : E;
+				*(p + dstPitch + 1) = H == F ? F : E;
+			} else {
+				*(p) = E;
+				*(p + 1) = E;
+				*(p + dstPitch) = E;
+				*(p + dstPitch + 1) = E;
+			}
+		}
+		dst += dstPitch * 2;
+		src += srcPitch;
+	}
+}
+
+void SDLStub::scale3x(uint16 *dst, uint16 dstPitch, const uint16 *src, uint16 srcPitch, uint16 w, uint16 h) {
+	dstPitch >>= 1;
+	while (h--) {
+		uint16 *p = dst;
+		for (int i = 0; i < w; ++i, p += 3) {
+			uint16 A = *(src + i - srcPitch - 1);
+			uint16 B = *(src + i - srcPitch);
+			uint16 C = *(src + i - srcPitch + 1);
+			uint16 D = *(src + i - 1);
+			uint16 E = *(src + i);
+			uint16 F = *(src + i + 1);
+			uint16 G = *(src + i + srcPitch - 1);
+			uint16 H = *(src + i + srcPitch);
+			uint16 I = *(src + i + srcPitch + 1);
+			if (B != H && D != F) {
+				*(p) = D == B ? D : E;
+				*(p + 1) = (D == B && E != C) || (B == F && E != A) ? B : E;
+				*(p + 2) = B == F ? F : E;
+				*(p + dstPitch) = (D == B && E != G) || (D == B && E != A) ? D : E;
+				*(p + dstPitch + 1) = E;
+				*(p + dstPitch + 2) = (B == F && E != I) || (H == F && E != C) ? F : E;
+				*(p + 2 * dstPitch) = D == H ? D : E;
+				*(p + 2 * dstPitch + 1) = (D == H && E != I) || (H == F && E != G) ? H : E;
+				*(p + 2 * dstPitch + 2) = H == F ? F : E;
+			} else {
+				*(p) = E;
+				*(p + 1) = E;
+				*(p + 2) = E;
+				*(p + dstPitch) = E;
+				*(p + dstPitch + 1) = E;
+				*(p + dstPitch + 2) = E;
+				*(p + 2 * dstPitch) = E;
+				*(p + 2 * dstPitch + 1) = E;
+				*(p + 2 * dstPitch + 2) = E;
+			}
 		}
 		dst += dstPitch * 3;
 		src += srcPitch;
